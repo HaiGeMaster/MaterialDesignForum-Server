9cf4073a5d72a000523ffdd7fb920c248a28a8fd
HaiGeMaster <2652549974@qq.com>
2024-10-11 11:06:39 +0800

    20241011110636
    20241011110636

---
+++ README.md
@@ -0,0 +1,6 @@
+# MaterialDesignForum-Server
+
+更新日志
+
+20241011:
+Notification修改为不再发送通知行为：自己到自己
--- src/Controllers/Notification.php
+++ src/Controllers/Notification.php
@@ -47,8 +47,12 @@ class Notification extends NotificationModel
         $reply_to_reply_id = 0
     ) {
         $is_add = false;
+        $notification = null;
 
-        if (self::IsVaildType($type) == true) {
+        if (
+            self::IsVaildType($type) == true &&
+            ($sender_id != $receiver_id)
+        ) {
 
             $notification = new NotificationModel;
             $notification->receiver_id = $receiver_id;
@@ -67,21 +71,21 @@ class Notification extends NotificationModel
                 UserController::AddNotificationCount($receiver_id);
 
                 //获取收件人邮箱
-                $receiver = UserController::where('user_id', $receiver_id)->first();
-                if($receiver != null){
+                $receiver = UserController::where('user_id', '=', $receiver_id)->first();
+                if ($receiver != null) {
                     $receiver_email = $receiver->email;
                     $receiver_language = $receiver->language;
 
                     $mail_title = i18n::t('Message.Client.Notifications.YouHaveNewNotifications', $receiver_language);
-                    $mail_content = i18n::t('Message.Client.Notifications.Type.'.$type, $receiver_language);
+                    $mail_content = i18n::t('Message.Client.Notifications.Type.' . $type, $receiver_language);
 
                     //把$mail_content里面的{value}替换成发送者的名字
                     $sender = UserController::where('user_id', $sender_id)->first();
-                    if($sender != null){
+                    if ($sender != null) {
                         $sender_name = $sender->username;
                         $mail_content = str_replace('{value}', $sender_name, $mail_content);
                     }
-                    
+
 
                     MailCaptchaModel::SendMail(
                         $receiver_email,
@@ -89,7 +93,6 @@ class Notification extends NotificationModel
                         $mail_content
                     );
                 }
-                
             }
         }
 
@@ -124,13 +127,13 @@ class Notification extends NotificationModel
             ->orderBy($field, $sort)
             ->paginate($per_page, ['*'], 'page', $page);
 
-            // return $notifications;
+        // return $notifications;
 
         if ($notifications != null) {
             foreach ($notifications as $key => $notification) {
                 UserController::SubNotificationCount($notification->receiver_id);
 
-                if($notification->read_time == 0){
+                if ($notification->read_time == 0) {
                     self::SetReadTime($notification->notification_id, Share::ServerTime());
                 }
 
@@ -143,7 +146,7 @@ class Notification extends NotificationModel
                     case 'question_answer':
                         $notification->question = QuestionController::where('question_id', $notification->question_id)->first();
                         $notification->answer = AnswerController::where('answer_id', $notification->answer_id)->first();
-                        if($notification->question != null && $notification->answer != null){
+                        if ($notification->question != null && $notification->answer != null) {
                             $receiver_content = $notification->question->title;
                             $sender_content = $notification->answer->content_markdown;
                         }
@@ -151,49 +154,49 @@ class Notification extends NotificationModel
                     case 'question_comment':
                         $notification->question = QuestionController::where('question_id', $notification->question_id)->first();
                         $notification->comment = CommentController::where('comment_id', $notification->comment_id)->first();
-                        if($notification->question != null && $notification->comment != null){
+                        if ($notification->question != null && $notification->comment != null) {
                             $receiver_content = $notification->question->title;
                             $sender_content = $notification->comment->content;
                         }
                         break;
                     case 'question_delete':
                         $notification->question = QuestionController::where('question_id', $notification->question_id)->first();
-                        if($notification->question != null){
+                        if ($notification->question != null) {
                             $receiver_content = $notification->question->title;
                         }
                         break;
                     case 'article_comment':
                         $notification->article = ArticleController::where('article_id', $notification->article_id)->first();
                         $notification->comment = CommentController::where('comment_id', $notification->comment_id)->first();
-                        if($notification->article != null && $notification->comment != null){
+                        if ($notification->article != null && $notification->comment != null) {
                             $receiver_content = $notification->article->title;
                             $sender_content = $notification->comment->content;
                         }
                         break;
                     case 'article_delete':
                         $notification->article = ArticleController::where('article_id', $notification->article_id)->first();
-                        if($notification->article != null){
+                        if ($notification->article != null) {
                             $receiver_content = $notification->article->title;
                         }
                         break;
                     case 'answer_comment':
                         $notification->answer = AnswerController::where('answer_id', $notification->answer_id)->first();
                         $notification->comment = CommentController::where('comment_id', $notification->comment_id)->first();
-                        if($notification->answer != null && $notification->comment != null){
+                        if ($notification->answer != null && $notification->comment != null) {
                             $receiver_content = $notification->answer->content_markdown;
                             $sender_content = $notification->comment->content;
                         }
                         break;
                     case 'answer_delete':
                         $notification->answer = AnswerController::where('answer_id', $notification->answer_id)->first();
-                        if($notification->answer != null){
+                        if ($notification->answer != null) {
                             $receiver_content = $notification->answer->content_markdown;
                         }
                         break;
                     case 'comment_reply':
                         $notification->comment = CommentController::where('comment_id', $notification->comment_id)->first();
                         $notification->reply = ReplyController::where('reply_id', $notification->reply_id)->first();
-                        if($notification->comment!=null){
+                        if ($notification->comment != null) {
                             switch ($notification->comment->commentable_type) {
                                 case 'article':
                                     $notification->article = ArticleController::where('article_id', $notification->comment->commentable_id)->first();
@@ -209,7 +212,7 @@ class Notification extends NotificationModel
                             // $sender_content = $notification->reply->content;
                         }
                         // $receiver_content = $notification->comment->content;
-                        if($notification->reply != null){
+                        if ($notification->reply != null) {
                             $sender_content = $notification->reply->content;
                         }
                         // $sender_content = $notification->reply->content;
@@ -228,11 +231,11 @@ class Notification extends NotificationModel
                             //         $notification->answer = AnswerController::where('answer_id', $notification->comment->commentable_id)->first();
                             //         break;
                             // }
-                            if($notification->comment->commentable_type == 'article'){
+                            if ($notification->comment->commentable_type == 'article') {
                                 $notification->article = ArticleController::where('article_id', $notification->comment->commentable_id)->first();
-                            }else if($notification->comment->commentable_type == 'question'){
+                            } else if ($notification->comment->commentable_type == 'question') {
                                 $notification->question = QuestionController::where('question_id', $notification->comment->commentable_id)->first();
-                            }else if($notification->comment->commentable_type == 'answer'){
+                            } else if ($notification->comment->commentable_type == 'answer') {
                                 $notification->answer = AnswerController::where('answer_id', $notification->comment->commentable_id)->first();
                             }
                             $receiver_content = $notification->comment->content;
@@ -252,11 +255,11 @@ class Notification extends NotificationModel
                             //         $notification->answer = AnswerController::where('answer_id', $notification->comment->commentable_id)->first();
                             //         break;
                             // }
-                            if($notification->comment->commentable_type == 'article'){
+                            if ($notification->comment->commentable_type == 'article') {
                                 $notification->article = ArticleController::where('article_id', $notification->comment->commentable_id)->first();
-                            }else if($notification->comment->commentable_type == 'question'){
+                            } else if ($notification->comment->commentable_type == 'question') {
                                 $notification->question = QuestionController::where('question_id', $notification->comment->commentable_id)->first();
-                            }else if($notification->comment->commentable_type == 'answer'){
+                            } else if ($notification->comment->commentable_type == 'answer') {
                                 $notification->answer = AnswerController::where('answer_id', $notification->comment->commentable_id)->first();
                             }
                             $notification->reply = ReplyController::where('reply_id', $notification->reply_id)->first(); //接收者的reply
@@ -290,11 +293,12 @@ class Notification extends NotificationModel
      * @param int $notification_id 通知ID
      * @return bool
      */
-    public static function DeleteNotification($user_token, $notification_id){
+    public static function DeleteNotification($user_token, $notification_id)
+    {
         $is_delete = false;
         $user_id = TokenController::GetUserId($user_token);
         $notification = NotificationModel::where('notification_id', $notification_id)->first();
-        if($notification != null && $notification->receiver_id == $user_id){
+        if ($notification != null && $notification->receiver_id == $user_id) {
             $notification->delete_time = Share::ServerTime();
             $is_delete = $notification->save();
         }
